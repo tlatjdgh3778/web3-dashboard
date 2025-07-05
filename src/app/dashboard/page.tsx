b93f13ca129136e5acd60f4832e46ef880108190
Seongho <tjdgh3778@gmail.com>
2025-07-06 00:02:07 +0900

    feat: add account info and balance loading functionality to dashboard
    - Use useAccount and useBalance hooks to fetch user account address and balance information
    - Display "Loading..." message while balance is being loaded

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useAccount, useBalance } from "wagmi";
+
 import GasTracker from "@/components/common/GasTracker";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
@@ -6,6 +10,11 @@ const standard = 0.303199859;
 const fast = 0.323199859;
 
 export default function DashboardPage() {
+	const { address } = useAccount();
+	const { data, isLoading } = useBalance({
+		address: address,
+	});
+
 	return (
 		<div className="flex flex-col flex-1">
 			<div className="grid grid-cols-3 gap-4">
@@ -14,7 +23,18 @@ export default function DashboardPage() {
 						<CardTitle>Total Portfolio Value</CardTitle>
 					</CardHeader>
 					<CardContent>
-						<p>1000</p>
+						{isLoading ? (
+							<p>Loading...</p>
+						) : (
+							<p>
+								{" "}
+								{JSON.stringify(
+									data,
+									(_, v) => (typeof v === "bigint" ? v.toString() : v),
+									2,
+								)}
+							</p>
+						)}
 					</CardContent>
 				</Card>
 				<Card className="w-full">
