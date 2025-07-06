f72511459f01af9350845c0eb85137d62630c2c7
Seongho <tjdgh3778@gmail.com>
2025-07-06 23:12:58 +0900

    feat: Add AddressInfo component
    - Add a new AddressInfo component that provides the ability to display address information and copy it to the clipboard
    - Get ENS name, cut address, include copy button

---
+++ src/components/common/AddressInfo.tsx
@@ -0,0 +1,46 @@
+import { Copy } from "lucide-react";
+import { useEnsName } from "wagmi";
+import { toast } from "sonner";
+
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+
+export default function AddressInfo({
+	address,
+}: {
+	address: `0x${string}` | undefined;
+}) {
+	const ensName = useEnsName({ address });
+
+	// copy address to clipboard
+	const handleCopyAddress = (address: string) => {
+		navigator.clipboard.writeText(address);
+		toast.success("Address copied to clipboard");
+	};
+	return (
+		<div className="flex items-center gap-2">
+			<p>{ensName.data || "UNNAMED"} </p>
+			<Badge variant="outline">
+				{address?.slice(0, 6)}...{address?.slice(-4)}
+			</Badge>
+			<Tooltip>
+				<TooltipTrigger>
+					<Button
+						variant="ghost"
+						className="text-muted-foreground cursor-pointer"
+						size="icon"
+						onClick={() => handleCopyAddress(address ?? "")}
+						asChild
+					>
+						<Copy className="w-4 h-4" />
+					</Button>
+				</TooltipTrigger>
+
+				<TooltipContent>
+					<p>Copy address</p>
+				</TooltipContent>
+			</Tooltip>
+		</div>
+	);
+}
