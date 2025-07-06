761fcb8c2a87d48ffef9d56c7624dd275dfa4bb9
Seongho <tjdgh3778@gmail.com>
2025-07-06 16:04:19 +0900

    feat: add remote image patterns to next.config.ts
    - Add remotePatterns for images from cdn.moralis.io and logo.moralis.io to next.config.ts for improved image handling.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.moralis.io',
+				port: '',
+				pathname: '/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'logo.moralis.io',
+				port: '',
+				pathname: '/**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
