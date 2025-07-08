ab0a9c269ce374c718718c5e672eb276a27036ae
Seongho <tjdgh3778@gmail.com>
2025-07-08 21:52:22 +0900

    feat: modify WalletHistory type
    - Remove the nullable type from WalletHistoryResponse and associated interfaces, and change all fields to string to increase consistency.
    - Improved code readability and enhanced type stability.

--- src/types/WalletHistory.ts
+++ src/types/WalletHistory.ts
@@ -1,8 +1,8 @@
 export interface WalletHistoryResponse {
 	page?: number;
 	page_size?: number;
-	limit?: string | null;
-	cursor?: string | null;
+	limit?: string;
+	cursor?: string;
 	result?: WalletHistory[];
 }
 
@@ -10,44 +10,44 @@ export interface WalletHistory {
 	hash?: string;
 	nonce?: string;
 	transaction_index?: string;
-	from_address_entity?: string | null;
-	from_address_entity_logo?: string | null;
+	from_address_entity?: string;
+	from_address_entity_logo?: string;
 	from_address?: string;
-	from_address_label?: string | null;
-	to_address_entity?: string | null;
-	to_address_entity_logo?: string | null;
+	from_address_label?: string;
+	to_address_entity?: string;
+	to_address_entity_logo?: string;
 	to_address?: string;
-	to_address_label?: string | null;
+	to_address_label?: string;
 	value?: string;
 	gas?: string;
 	gas_price?: string;
 	receipt_cumulative_gas_used?: string;
 	receipt_gas_used?: string;
-	receipt_contract_address?: string | null;
-	receipt_root?: string | null;
-	receipt_status?: string | null;
-	block_timestamp?: string | null;
-	block_number?: string | null;
-	block_hash?: string | null;
-	transaction_fee?: string | null;
-	method_label?: string | null;
-	internal_transactions?: InternalTransaction[] | null;
-	nft_transfers?: NftTransfer[] | null;
-	erc20_transfers?: Erc20Transfer[] | null;
-	native_transfers?: NativeTransfer[] | null;
-	contract_interactions?: ContractInteraction | null;
-	summary?: string | null;
+	receipt_contract_address?: string;
+	receipt_root?: string;
+	receipt_status?: string;
+	block_timestamp?: string;
+	block_number?: string;
+	block_hash?: string;
+	transaction_fee?: string;
+	method_label?: string;
+	internal_transactions?: InternalTransaction[];
+	nft_transfers?: NftTransfer[];
+	erc20_transfers?: Erc20Transfer[];
+	native_transfers?: NativeTransfer[];
+	contract_interactions?: ContractInteraction;
+	summary?: string;
 	possible_spam?: boolean;
-	category?: string | null;
+	category?: string;
 }
 
 export interface InternalTransaction {
 	transaction_hash?: string;
 	block_number?: string;
-	block_hash?: string | null;
+	block_hash?: string;
 	type?: string; // CALL, CREATE, etc.
-	from?: string | null;
-	to?: string | null;
+	from?: string;
+	to?: string;
 	value?: string;
 	gas?: string;
 	gas_used?: string;
@@ -58,25 +58,25 @@ export interface InternalTransaction {
 export interface NftTransfer {
 	token_address?: string;
 	token_id?: string;
-	from_address_entity?: string | null;
-	from_address_entity_logo?: string | null;
+	from_address_entity?: string;
+	from_address_entity_logo?: string;
 	from_address?: string;
-	from_address_label?: string | null;
-	to_address_entity?: string | null;
-	to_address_entity_logo?: string | null;
+	from_address_label?: string;
+	to_address_entity?: string;
+	to_address_entity_logo?: string;
 	to_address?: string;
-	to_address_label?: string | null;
+	to_address_label?: string;
 	value?: string;
 	amount?: string;
 	contract_type?: string; // ERC721, etc.
-	block_number?: string | null;
-	block_timestamp?: string | null;
-	block_hash?: string | null;
+	block_number?: string;
+	block_timestamp?: string;
+	block_hash?: string;
 	transaction_hash?: string;
 	transaction_type?: string;
 	transaction_index?: number;
 	log_index?: number;
-	operator?: string | null;
+	operator?: string;
 	possible_spam?: boolean;
 	verified_collection?: boolean;
 }
@@ -91,33 +91,33 @@ export interface Erc20Transfer {
 	block_timestamp?: string;
 	block_number?: string;
 	block_hash?: string;
-	to_address_entity?: string | null;
-	to_address_entity_logo?: string | null;
+	to_address_entity?: string;
+	to_address_entity_logo?: string;
 	to_address?: string;
-	to_address_label?: string | null;
-	from_address_entity?: string | null;
-	from_address_entity_logo?: string | null;
+	to_address_label?: string;
+	from_address_entity?: string;
+	from_address_entity_logo?: string;
 	from_address?: string;
-	from_address_label?: string | null;
+	from_address_label?: string;
 	value?: string;
 	transaction_index?: number;
 	log_index?: number;
 	possible_spam?: boolean;
 	verified_contract?: boolean;
-	security_score?: number | null;
+	security_score?: number;
 	direction?: string;
 	value_formatted?: string;
 }
 
 export interface NativeTransfer {
-	from_address_entity?: string | null;
-	from_address_entity_logo?: string | null;
+	from_address_entity?: string;
+	from_address_entity_logo?: string;
 	from_address?: string;
-	from_address_label?: string | null;
-	to_address_entity?: string | null;
-	to_address_entity_logo?: string | null;
-	to_address?: string | null;
-	to_address_label?: string | null;
+	from_address_label?: string;
+	to_address_entity?: string;
+	to_address_entity_logo?: string;
+	to_address?: string;
+	to_address_label?: string;
 	value?: string;
 	value_formatted?: string;
 	direction?: string; // incoming | outgoing
@@ -131,19 +131,19 @@ export interface ContractInteraction {
 }
 
 export interface Approvals {
-	value?: string | null;
-	value_formatted?: string | null;
+	value?: string;
+	value_formatted?: string;
 	token?: {
 		address?: string;
-		address_label?: string | null;
-		token_name?: string | null;
-		token_logo?: string | null;
-		token_symbol?: string | null;
+		address_label?: string;
+		token_name?: string;
+		token_logo?: string;
+		token_symbol?: string;
 	};
 	spender?: {
 		address?: string;
-		address_label?: string | null;
-		entity?: string | null;
-		entity_logo?: string | null;
+		address_label?: string;
+		entity?: string;
+		entity_logo?: string;
 	};
 }
