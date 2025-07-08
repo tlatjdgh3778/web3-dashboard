f07fd2d357092205d673555758f57c9fffd6401b
Seongho <tjdgh3778@gmail.com>
2025-07-08 21:55:09 +0900

    fe: Add new columns in the Transactive Transaction table
    - And then display details of the recent transaction, from adding the details of the recent transaction.
    - Implementing a cell rendering logic for each column, transmitter address, the receiver address, the receiver address, and block time staff.

--- src/app/dashboard/components/recent-transactions/columns.tsx
+++ src/app/dashboard/components/recent-transactions/columns.tsx
@@ -3,7 +3,12 @@
 import { DataTableColumnHeader } from "@/components/common/data-table/table-header";
 import type { WalletHistory } from "@/types/WalletHistory";
 import type { ColumnDef } from "@tanstack/react-table";
+
 import { TransactionHash } from "./transaction-hash";
+import Method from "./method";
+import FromAddress from "./from-address";
+import ToAddress from "./to-address";
+import Timestamp from "@/components/common/timestamp";
 
 export const columns: ColumnDef<WalletHistory>[] = [
 	{
@@ -19,12 +24,19 @@ export const columns: ColumnDef<WalletHistory>[] = [
 		header: ({ column }) => (
 			<DataTableColumnHeader column={column} title="Method" />
 		),
+		cell: ({ row }) => <Method method={row.original.method_label ?? "N/A"} />,
 		enableSorting: false,
 	},
 	{
 		accessorKey: "from_address",
 		header: ({ column }) => (
 			<DataTableColumnHeader column={column} title="From" />
+		),
+		cell: ({ row }) => (
+			<FromAddress
+				from_address={row.original?.from_address as string}
+				from_address_label={row.original?.from_address_label ?? undefined}
+			/>
 		),
 		enableSorting: false,
 	},
@@ -33,6 +45,20 @@ export const columns: ColumnDef<WalletHistory>[] = [
 		header: ({ column }) => (
 			<DataTableColumnHeader column={column} title="To" />
 		),
+		cell: ({ row }) => (
+			<ToAddress
+				to_address={row.original?.to_address as string}
+				to_address_label={row.original?.to_address_label ?? undefined}
+			/>
+		),
+		enableSorting: false,
+	},
+	{
+		accessorKey: "block_timestamp",
+		header: ({ column }) => (
+			<DataTableColumnHeader column={column} title="Block Timestamp" />
+		),
+		cell: ({ row }) => <Timestamp timestamp={row.original?.block_timestamp} />,
 		enableSorting: false,
 	},
 ];
