bceede5ff6c99eb61833555763c50c0c044430a7
Seongho <tjdgh3778@gmail.com>
2025-07-13 13:34:30 +0900

    feat: Added new mock data files
    - mockBalance.ts: Added mock balance data
    - mockTokenPrice.ts: Added mock data for different token prices
    - mockTotalBalances.ts: Added mock data for total balances of different assets

---
+++ src/mock/data/mockBalance.ts
@@ -0,0 +1,4 @@
+export const mockBalance = {
+	_hex: "0x01b63659576656",
+	_isBigNumber: true,
+};
---
+++ src/mock/data/mockTokenPrice.ts
@@ -0,0 +1,37 @@
+export const mockTokenPrice = {
+	eth: {
+		usd: 2953.59,
+		usd_market_cap: 356286731912.2218,
+		usd_24h_vol: 15473227614.047422,
+		usd_24h_change: -0.2280335902735959,
+		last_updated_at: 1752375794,
+	},
+	sol: {
+		usd: 161.19,
+		usd_market_cap: 86406670177.81583,
+		usd_24h_vol: 7225877241.190057,
+		usd_24h_change: -1.3043628424139075,
+		last_updated_at: 1752375787,
+	},
+	usdc: {
+		usd: 0.999902,
+		usd_market_cap: 63627680517.58577,
+		usd_24h_vol: 3543283270.2143245,
+		usd_24h_change: -0.0024723948233348475,
+		last_updated_at: 1752375788,
+	},
+	weth: {
+		usd: 2955.87,
+		usd_market_cap: 7307533672.325681,
+		usd_24h_vol: 62935542.51256236,
+		usd_24h_change: -0.13841413541866476,
+		last_updated_at: 1752375788,
+	},
+	wavax: {
+		usd: 21.07,
+		usd_market_cap: 310609327.25021404,
+		usd_24h_vol: 21819252.3213766,
+		usd_24h_change: 0.5583128492044667,
+		last_updated_at: 1752375788,
+	},
+};
