31bb4e1500be45d48209d40834812e308b479c79
Seongho <tjdgh3778@gmail.com>
2025-07-17 22:11:22 +0900

    feat: Improved portfolio page and layout
    - layout.tsx: Added address information to the portfolio layout, improved basic structure to improve user experience
    - page.tsx: Added total balance component to the portfolio page, improved layout to enhance visual elements
    - TotalBalances.tsx: Added total balance and change rate calculation logic, implemented loading state handling and data display functions
    - useGetTokenMarketData.ts: Added custom hook to get token market data
    - mockTokenMarketData.ts: Added mock data for 7-day token market data
    - PriceChangePercentage.ts, TokenMarketData.ts: Added price change rate and token market data type definitions

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,3 +1,20 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { mockTotalBalance } from "@/mock/data/mockTotalBalances";
+
+import TotalBalances from "./components/TotalBalances";
+// import TotalBalances from "../dashboard/components/total-balances/total-balances";
+
 export default function PortfolioPage() {
-	return <div>Portfolio</div>;
+	const walletBalances = mockTotalBalance;
+	return (
+		<div className="flex flex-col flex-1 space-y-6">
+			<div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+				<Card className="lg:col-span-1">
+					<CardContent>
+						<TotalBalances walletBalances={walletBalances} isLoading={false} />
+					</CardContent>
+				</Card>
+			</div>
+		</div>
+	);
 }
