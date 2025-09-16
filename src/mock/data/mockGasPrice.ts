bf529bbbca6cfde86cf75c0f8e401377da0d1757
Seongho <tjdgh3778@gmail.com>
2025-09-16 23:02:13 +0900

    feat: mockGasPrice 데이터 추가
    - src/mock/data/mockGasPrice.ts: 가스 가격 관련 모의 데이터 추가.

---
+++ src/mock/data/mockGasPrice.ts
@@ -0,0 +1,9 @@
+export const mockGasPrice = {
+	LastBlock: "23368922",
+	SafeGasPrice: "500.789968522",
+	ProposeGasPrice: "50.799968522",
+	FastGasPrice: "50.079965374",
+	suggestBaseFee: "50.789968522",
+	gasUsedRatio:
+		"0.4258816,0.425917977777778,0.446662291727726,0.515874981836094,0.307924733333333",
+};
