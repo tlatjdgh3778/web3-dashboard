91f511acde1ba3798a3461a4424e096a8eb0032b
Seongho <tjdgh3778@gmail.com>
2025-09-18 23:07:12 +0900

    feat: QueryClientProvider 제거 및 레이아웃 구조 단순화
    - layout-provider.tsx: QueryClientProvider를 제거하고, RainbowKitProvider를 직접 사용하여 레이아웃 구조를 간소화함.

--- src/components/common/layout-provider.tsx
+++ src/components/common/layout-provider.tsx
@@ -1,14 +1,11 @@
 "use client";
 
 import { WagmiProvider } from "wagmi";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 import "@rainbow-me/rainbowkit/styles.css";
 
 import config from "@/lib/walletConfig";
-
-const queryClient = new QueryClient();
 
 export default function LayoutProvider({
 	children,
@@ -17,9 +14,7 @@ export default function LayoutProvider({
 }) {
 	return (
 		<WagmiProvider config={config}>
-			<QueryClientProvider client={queryClient}>
-				<RainbowKitProvider>{children}</RainbowKitProvider>
-			</QueryClientProvider>
+			<RainbowKitProvider>{children}</RainbowKitProvider>
 		</WagmiProvider>
 	);
 }
