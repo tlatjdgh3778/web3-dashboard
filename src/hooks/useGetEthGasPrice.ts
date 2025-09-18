083cb2cbff7932c1017754909a135261a133faad
Seongho <tjdgh3778@gmail.com>
2025-09-18 23:08:01 +0900

    feat: 가스 가격 조회 함수 분리 및 코드 구조 개선
    - useGetEthGasPrice.ts: 가스 가격 조회 로직을 fetchEthGasPrice 함수로 분리하여 코드 가독성을 향상시킴. API 키 설정을 함수 내부로 이동하여 코드의 재사용성을 높임.

--- src/hooks/useGetEthGasPrice.ts
+++ src/hooks/useGetEthGasPrice.ts
@@ -11,17 +11,17 @@ export default function useGetEthGasPrice(): UseQueryResult<
 	EthGasPrice,
 	Error
 > {
-	const API_KEY = process.env.NEXT_PUBLIC_ETHSCAN_API_KEY;
-
 	return useQuery({
 		queryKey: ["ethGasPrice"],
-		queryFn: async () => {
-			const response = await fetch(
-				`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${API_KEY}`,
-			);
-			const data = await response.json();
-			return data.result;
-		},
-		refetchInterval: 60 * 1000 * 30,
+		queryFn: fetchEthGasPrice,
 	});
 }
+
+export const fetchEthGasPrice = async () => {
+	const API_KEY = process.env.NEXT_PUBLIC_ETHSCAN_API_KEY;
+	const response = await fetch(
+		`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${API_KEY}`,
+	);
+	const data = await response.json();
+	return data.result;
+};
