2357a6147358f50f75297f9cf24c74b1be9e3f19
Seongho <tjdgh3778@gmail.com>
2025-11-13 20:57:57 +0900

    refactor: 코드 스타일 일관성을 위한 들여쓰기 수정
    - tx-fee.tsx: 코드의 들여쓰기를 일관되게 수정하여 가독성을 향상시킴.

--- src/components/common/data-table-column/tx-fee.tsx
+++ src/components/common/data-table-column/tx-fee.tsx
@@ -4,19 +4,23 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useGetTransactionFee } from "@/hooks/useGetTransactionFee";
 
 export default function TxFee({ hash }: { hash: string }) {
-    return (
-        <Suspense fallback={<TxFeeSkeleton />}>
-            <TxFeeContent hash={hash} />
-        </Suspense>
-    );
+	return (
+		<Suspense fallback={<TxFeeSkeleton />}>
+			<TxFeeContent hash={hash} />
+		</Suspense>
+	);
 }
 
 const TxFeeContent = ({ hash }: { hash: string }) => {
-    const { data: txFee } = useGetTransactionFee({ hash });
+	const { data: txFee } = useGetTransactionFee({ hash });
 
-    return <div className="text-sm text-muted-foreground">{txFee?.txFeeEth.toFixed(6)} ETH</div>;
+	return (
+		<div className="text-sm text-muted-foreground">
+			{txFee?.txFeeEth.toFixed(6)} ETH
+		</div>
+	);
 };
 
 const TxFeeSkeleton = () => {
-    return <Skeleton className="h-4 w-10" />;
+	return <Skeleton className="h-4 w-10" />;
 };
