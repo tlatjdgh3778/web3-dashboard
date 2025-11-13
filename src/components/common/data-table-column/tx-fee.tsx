8d3317de525f13ba733b5d9c142c3639c42959d7
Seongho <tjdgh3778@gmail.com>
2025-11-13 20:57:19 +0900

    feat: tx-fee 컴포넌트에서 mock 데이터 제거 및 실제 데이터 가져오기 로직 추가
    - tx-fee.tsx: useGetTransactionFee 훅을 사용하여 거래 수수료 데이터를 가져오도록 수정하고, 불필요한 mock 데이터 관련 코드를 제거함.

--- src/components/common/data-table-column/tx-fee.tsx
+++ src/components/common/data-table-column/tx-fee.tsx
@@ -1,28 +1,22 @@
 import { Suspense } from "react";
 
 import { Skeleton } from "@/components/ui/skeleton";
-// import { useGetTransactionFee } from "@/hooks/useGetTransactionFee";
-import { getMockTransactionReceipt } from "@/mock/data/mockTransactionReceipt";
+import { useGetTransactionFee } from "@/hooks/useGetTransactionFee";
 
 export default function TxFee({ hash }: { hash: string }) {
-	return (
-		<Suspense fallback={<TxFeeSkeleton />}>
-			<TxFeeContent hash={hash} />
-		</Suspense>
-	);
+    return (
+        <Suspense fallback={<TxFeeSkeleton />}>
+            <TxFeeContent hash={hash} />
+        </Suspense>
+    );
 }
 
 const TxFeeContent = ({ hash }: { hash: string }) => {
-	// const { data: txFee } = useGetTransactionFee({ hash });
-	const txFee = getMockTransactionReceipt(hash);
+    const { data: txFee } = useGetTransactionFee({ hash });
 
-	return (
-		<div className="text-sm text-muted-foreground">
-			{txFee?.txFeeEth.toFixed(6)} ETH
-		</div>
-	);
+    return <div className="text-sm text-muted-foreground">{txFee?.txFeeEth.toFixed(6)} ETH</div>;
 };
 
 const TxFeeSkeleton = () => {
-	return <Skeleton className="h-4 w-10" />;
+    return <Skeleton className="h-4 w-10" />;
 };
