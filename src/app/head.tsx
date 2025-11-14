96f78ca66424caf779354079a8ca25361fe6d9e1
Seongho <tjdgh3778@gmail.com>
2025-11-14 21:27:59 +0900

    feat: implement dynamic generation of iOS startup images for PWA

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+import { generateIOSStartupImages } from "./_pwa/ios-startup-images";
+
+export default function Head() {
+    const images = generateIOSStartupImages();
+
+    return (
+        <>
+            {Object.entries(images).map(([key, value]) => {
+                const { media, href } = JSON.parse(value);
+                return <link key={key} rel="apple-touch-startup-image" media={media} href={href} />;
+            })}
+        </>
+    );
+}
